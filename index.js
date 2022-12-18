0460489a2f70bbb193bd37b78d433d6bc5e0485c
mhurchaidh <bmurchie1997@gmail.com>
2022-12-17 20:21:35 -0800

    commit tree d8ba51e232794f2c197571542dd4ca4bf0a011ae

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    const driverMatch = drivers.filter(drivers => drivers.toUpperCase() === string.toUpperCase());
+    return !!driverMatch ? driverMatch : undefined;
+}
+
+function fuzzyMatch(drivers, string) {
+    const driverMatch = drivers.filter(drivers => drivers.substring(0, 2) === string);
+    return !!driverMatch ? driverMatch : undefined;
+}
+
+function matchName(drivers, string) {
+    const driverMatch = drivers.filter(drivers => drivers.name === string);
+    return !!driverMatch ? driverMatch : undefined;
+}
